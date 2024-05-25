95aaf9f63f4e6405f2edc5d39712d717ce22f36e
NgocMinhThuNguyen <94945964+NgocMinhThuNguyen@users.noreply.github.com>
2024-05-25 19:32:19 +0800

    add header markup

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <section>
+      <h1>Reliable, efficient delivery <span>Powered by Technology</span></h1>
+      <p>Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
+    </section>
+  )
+}
+
+export default Header
